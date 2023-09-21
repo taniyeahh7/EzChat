6b4623a77d37a87097e47357abce7e623d9ab709
anisharma1 <111197294+anisharma1@users.noreply.github.com>
2023-09-22 01:54:46 +0530

    Cleaning

--- illumina/chat.py
+++ illumina/chat.py
@@ -1,24 +1,31 @@
 import random
 import json
-from model import NeuralNet
+
 import torch
+
+from model import NeuralNet
 from nltk_utils import bag_of_words, tokenize
 
-device = 'cpu'
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-with open('queries.json', 'r') as json_data:
-    queries = json.load(json_data)
+with open('intents.json', 'r') as json_data:
+    intents = json.load(json_data)
 
-data = torch.load('data.pth')
+FILE = "data.pth"
+data = torch.load(FILE)
+
+input_size = data["input_size"]
+hidden_size = data["hidden_size"]
+output_size = data["output_size"]
 all_words = data['all_words']
+tags = data['tags']
 model_state = data["model_state"]
 
-model = NeuralNet(data["input_size"], data["hidden_size"], data["output_size"]).to(device)
+model = NeuralNet(input_size, hidden_size, output_size).to(device)
 model.load_state_dict(model_state)
 model.eval()
 
-tags = data['tags']
-print("Let's chat! (type 'quit' to exit)")
+bot_name = "Sam"
 
 def get_response(msg):
     sentence = tokenize(msg)
@@ -34,10 +41,21 @@ def get_response(msg):
     probs = torch.softmax(output, dim=1)
     prob = probs[0][predicted.item()]
     if prob.item() > 0.75:
-        for query in queries['queries']:
-            if tag == query["tag"]:
-                return random.choice(query['responses'])
+        for intent in intents['intents']:
+            if tag == intent["tag"]:
+                return random.choice(intent['responses'])
     
     return "I do not understand..."
 
-    
+
+if __name__ == "__main__":
+    print("Let's chat! (type 'quit' to exit)")
+    while True:
+        # sentence = "do you use credit cards?"
+        sentence = input("You: ")
+        if sentence == "quit":
+            break
+
+        resp = get_response(sentence)
+        print(resp)
+
